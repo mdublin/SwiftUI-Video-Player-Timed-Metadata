e2532d15f937d5ce69910c4a36469868345577a5
mdublin <alegionofgenius@gmail.com>
2022-07-25 11:48:25 -0400

    Initial Commit

---
+++ Shared/TimedMetadataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimedMetadataDemoApp.swift
+//  Shared
+//
+//  Created by Matt Dublin on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TimedMetadataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
